b00b4ff4b7b260274a4335ebf0b9bade1a140cd3
Kevin Patoiya <Kevinbpatoliya23@gmail.com>
2022-08-09 18:34:09 +0530

    Bottom Navigation

---
+++ app/src/main/java/bigcomponet/hwork/bigcomponet/Activity/Typeof_Menu_Activity.java
@@ -0,0 +1,17 @@
+package bigcomponet.hwork.bigcomponet.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bigcomponet.hwork.bigcomponet.R;
+
+public class Typeof_Menu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_typeof_menu);
+
+    }
+}
